ab8d78693d632d86de2001763c9f38febce0899d
Abrarulctg <jubairctg99@gmail.com>
2024-08-17 02:51:48 +0600

    Added filter by BrandName and By Category

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -5,7 +5,7 @@ import { FaRegClock, FaStar } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
 
 const Products = () => {
-    // const products = useLoaderData();
+    const allProducts = useLoaderData();
     const { register, handleSubmit, formState: { errors }, } = useForm();
 
     const [products, setProducts] = useState([]);
@@ -15,16 +15,28 @@ const Products = () => {
     const [count, setCount] = useState(0);
     const [searchQuery, setSearchQuery] = useState('');
     const [sortOrder, setSortOrder] = useState('');
-    // const [bnands, setBrands] = useState([]);
-    // const [categories, setCategories] = useState([]);
-    // const [priceRange, setPriceRange] = useState([0, 100]);
+    const [brands, setBrands] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [priceRange, setPriceRange] = useState([0, 100]);
 
-    // const [selectedBrand, setSelectedBrand] = useState('');
-    // const [selectedCategory, setSelectedCategory] = useState('');
+    const [selectedBrand, setSelectedBrand] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     const numberOfPages = Math.ceil(count / productsPerPage);
     const pages = [...Array(numberOfPages).keys()];
 
+    //Getting  Categories
+    useEffect(() => {
+        fetch('http://localhost:5000/products/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, [])
+    //Getting brandName 
+    useEffect(() => {
+        fetch('http://localhost:5000/products/brands')
+            .then(res => res.json())
+            .then(data => setBrands(data));
+    }, [])
 
     useEffect(() => {
         fetch('http://localhost:5000/productCount')
@@ -33,8 +45,33 @@ const Products = () => {
     }, [])
 
 
+    // useEffect(() => {
+    //     let url = `http://localhost:5000/products?page=${currentPage}&size=${productsPerPage}`
+    //     if (sortOrder) {
+    //         fetch(url + `&sortOrder=${sortOrder}`)
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 setProducts(data);
+    //                 console.log(data);
+    //             })
+    //     }
+    //     else {
+    //         fetch(url)
+    //             .then(res => res.json())
+    //             .then(data => setProducts(data))
+    //     }
+    // }, [currentPage, productsPerPage, searchQuery, sortOrder]);
+
+
     useEffect(() => {
         let url = `http://localhost:5000/products?page=${currentPage}&size=${productsPerPage}`
+        if (selectedBrand) {
+            url = url + `&brand=${selectedBrand}`;
+        }
+        if (selectedCategory) {
+            url = url + `&category=${selectedCategory}`;
+        }
+
         if (sortOrder) {
             fetch(url + `&sortOrder=${sortOrder}`)
                 .then(res => res.json())
@@ -43,12 +80,20 @@ const Products = () => {
                     console.log(data);
                 })
         }
+        if (priceRange) {
+            url = url + `&minPrice=${priceRange[0]}&maxPrice=${priceRange[1]}`;
+            console.log(url)
+        }
+
         else {
             fetch(url)
                 .then(res => res.json())
                 .then(data => setProducts(data))
         }
-    }, [currentPage, productsPerPage, searchQuery, sortOrder]);
+
+    }, [currentPage, productsPerPage, searchQuery, sortOrder, selectedBrand, selectedCategory, priceRange]);
+
+
 
 
 
@@ -82,6 +127,28 @@ const Products = () => {
             })
     }
 
+
+
+    const handleBrandChange = (e) => {
+        setSelectedBrand(e.target.value);
+        setCurrentPage(0);
+
+    }
+
+    const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value);
+        setCurrentPage(0);
+        console.log(selectedCategory)
+    }
+
+    const handlePriceRangeChange = (e) => {
+        const range = e.target.value;
+        setPriceRange(range);
+        setCurrentPage(0)
+        console.log(range)
+    }
+
+
     function convertToLocalTime(utcDateTime) {
         const date = new Date(utcDateTime);
         const localTime = date.toLocaleString();
@@ -105,7 +172,7 @@ const Products = () => {
         }
     }
 
-
+    // console.log(brands)
 
     // console.log(products)
     return (
@@ -129,6 +196,9 @@ const Products = () => {
                 </form> */}
                 <form onSubmit={handleSearch}>
                     <div className="flex justify-center items-center mt-8 mx-10 gap-2">
+                        <div>
+                            <button className='btn btn-primary' onClick={handleViewAll}>View All</button>
+                        </div>
                         <label className="form-control w-full lg:w-1/3 ">
                             <input
                                 type="text"
@@ -139,40 +209,62 @@ const Products = () => {
                         </label>
                         <button className='btn btn-success'>Search</button>
                     </div>
-                    {/* <div className="flex justify-center">
-                        {errors.searchText && <span className='text-red-400 mt-2'>Please write a Product Name</span>}
-                    </div> */}
+
                 </form>
             </div>
-            <div className='flex gap-2 mx-4 my-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mx-4 my-4'>
 
-                <div>
-                    <select className="select select-primary w-full max-w-xs" onChange={handleSortOrderChange}>
-                        <option disabled selected>Sort By:</option>
+                <div className='flex items-center'>
+                    <h3 className='text-base font-semibold w-1/3'>Sort By:</h3>
+                    <select className="select select-primary w-full" onChange={handleSortOrderChange}>
+                        <option defaultValue="">Select an option:</option>
                         <option value="priceAsc">Price Low to High</option>
                         <option value="priceDesc">Price High to Low</option>
                         <option value="dateDesc">Newest First</option>
                         <option value="dateAsc">Newest Last</option>
                     </select>
                 </div>
-                <button className='btn' disabled>View All</button>
 
-                <div>
-                    <select className="select select-primary w-full max-w-xs">
-                        <option disabled selected>Sort By:</option>
-                        <option value="priceAsc">Price Low to High</option>
-                        <option value="priceDesc">Price High to Low</option>
-                        <option value="dateDesc">Newest First</option>
-                        <option value="dateAsc">Newest Last</option>
+                {/* Filtering */}
+                <div className='flex items-center gap-2'>
+                    <h3 className='text-base font-semibold w-1/3 ml-4 mr-2'>Filter:</h3>
+                    {/* Filter by Brand Name */}
+                    <select className="select select-primary w-full" onChange={handleBrandChange}>
+                        <option disabled selected>By Brand</option>
+                        {brands.map((brand, idx) => <option key={idx} value={brand}>{brand}</option>)}
                     </select>
                 </div>
                 <div>
-                    <button className='btn btn-primary' onClick={handleViewAll}>View All</button>
+                    {/* Filter by Category */}
+                    <select className="select select-primary w-full max-w-xs" onChange={handleCategoryChange}>
+                        <option disabled selected>By Category</option>
+                        {categories.map((category, idx) => <option key={idx} value={category}>{category}</option>)}
+                    </select>
                 </div>
+                <div className='flex items-center'>
+                    {/* Filter by PriceRange */}
+                    <label className='text-base'>PriceRange:</label>
+                    {/* <input type="range" min="5" max="100000" step="5" onChange={handlePriceRangeChange} /> */}
+                    <select className="select select-primary w-full" onChange={handlePriceRangeChange}>
+                        {/* <option >Select an option:</option> */}
+                        <option defaultValue="" >All Prices</option>
+                        <option value="0-50">0 - 50</option>
+                        <option value="50-100">50 - 100</option>
+                        <option value="100-200">100 - 200</option>
+                        <option value="200-2000">200 - 2000</option>
+                        <option value="2000-10000">200 - 10000</option>
+                    </select>
+
+
+                </div>
+
                 <div>
 
                 </div>
             </div>
+
+
+            {/* Product Cards */}
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5 '>
                 {products.length <= 0 ? <div className='' ><span className='text-red-400 text-3xl text-center mt-2'>No Product Found</span></div> :
                     products.map((product) => <div className="card card-compact bg-base-100 shadow-xl mx-4" key={product._id}>
@@ -199,7 +291,7 @@ const Products = () => {
 
             </div>
 
-
+            {/* Pagination */}
             <div className='pagination my-8 mx-4 text-base'>
                 <div className="flex flex-col lg:flex-row justify-center place-content-center lg:justify-between">
                     <div className='flex justify-center items-center mb-4'>
